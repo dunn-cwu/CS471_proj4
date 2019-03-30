975e17a3eeeb8fa1c50a0cbaf53eae5dd7da4ab4
Andrew Dunn <andrew.dunn@cwu.edu>
2019-03-29 17:29:49 -0700

    Modified cmake

--- build-debug.sh
+++ build-debug.sh
@@ -1,5 +1,6 @@
+mkdir -p build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug .
+cmake -DCMAKE_BUILD_TYPE=Debug ../
 make
 mkdir -p debug
 mv ./cs471_proj1.out debug/cs471_proj1.out
--- build-release.sh
+++ build-release.sh
@@ -1,6 +1,7 @@
+mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Release .
+cmake -DCMAKE_BUILD_TYPE=Release ../
 make
 mkdir -p release
 mv ./cs471_proj1.out release/cs471_proj1.out
-echo Program vinary moved to build/release
+echo Program binary moved to build/release
