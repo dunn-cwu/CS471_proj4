11ca0cc2189b684c98086ebad8a443aa52963f10
Andrew Dunn <DunnA@cwu.edu>
2019-04-18 11:04:33 -0700

    Fixed windows scripts

--- win-build-release.bat
+++ win-build-release.bat
@@ -3,5 +3,5 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
 mingw32-make
 mkdir release
-move /y cs471_proj1.out.exe release\cs471_proj1.out.exe
+move /y cs471_proj2.out.exe release\cs471_proj2.out.exe
 cmd /k
--- win-run-all.bat
+++ win-run-all.bat
@@ -1,7 +1,7 @@
-build\release\cs471_proj1.out iparam-10dim.ini
+build\release\cs471_proj2.out iparam-10dim.ini
 
-build\release\cs471_proj1.out iparam-20dim.ini
+build\release\cs471_proj2.out iparam-20dim.ini
 
-build\release\cs471_proj1.out iparam-30dim.ini
+build\release\cs471_proj2.out iparam-30dim.ini
 
 cmd /k
